82fddadc184706e1f1c5dcea8d45f6e75f827e55
Akkala Harish Goud <73022679+harishgoudakkala@users.noreply.github.com>
2026-08-16 12:44:27 +0530

    added nelify config

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -9,7 +9,7 @@ import axios from 'axios'
 import { Toaster } from 'react-hot-toast'
 import "katex/dist/katex.min.css";
 
-axios.defaults.baseURL = 'https://ai-chatbot-18wr.onrender.com/api/v1'
+axios.defaults.baseURL = '/api/v1'
 axios.defaults.withCredentials = true
 
 const styles = createTheme({
